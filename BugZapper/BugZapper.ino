7153daff01eb93495e64da114ec78a99c1be754b
Christopher DuHadway <christopherduhadway@gmail.com>
2017-10-09 14:39:39 -0600

    Initial Commit of Bug Zapper project

---
+++ BugZapper/BugZapper.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BugZapper.ino
+ Created:	10/9/2017 1:32:53 PM
+ Author:	Christopher DuHadway
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
